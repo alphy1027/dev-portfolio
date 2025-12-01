08764e0285d9f6fb2ecb1ac67f25277b97348a08
alphy1027 <alphy1027@gmail.com>
2025-12-01 15:14:24 +0300

    added padding to portfolio section

--- src/app/home/sections/AboutSection.tsx
+++ src/app/home/sections/AboutSection.tsx
@@ -6,19 +6,21 @@ import Image from "next/image";
 
 export default function AboutSection() {
   return (
-    <SectionContainer className="flex flex-col items-center gap-space-4">
+    <SectionContainer className="flex flex-col items-center gap-space-7">
       <SectionTitle caption="About me">Know more about me</SectionTitle>
-      <Image
-        src="/about-image.jpg"
-        alt="profile image"
-        width={190}
-        height={200}
-        sizes="190px"
-        className="bg-neutral-light object-cover rounded-surface-radius w-[190px] h-[200px]"
-      />
-      <p className="text-center max-w-[510px] md:max-w-[634px]">{about.body}</p>
-      <p className="text-lg font-semibold">Below are links to my Resume</p>
-      <ResumeLinksSection />
+      <div className="flex flex-col items-center gap-space-4">
+        <Image
+          src="/about-image.jpg"
+          alt="profile image"
+          width={190}
+          height={200}
+          sizes="190px"
+          className="bg-neutral-light object-cover rounded-surface-radius w-[190px] h-[200px]"
+        />
+        <p className="text-center max-w-[510px] md:max-w-[634px]">{about.body}</p>
+        <p className="text-lg font-semibold">Below are links to my Resume</p>
+        <ResumeLinksSection />
+      </div>
     </SectionContainer>
   );
 }
--- src/app/home/sections/PortfolioSection.tsx
+++ src/app/home/sections/PortfolioSection.tsx
@@ -5,9 +5,9 @@ import ProjectCard from "../components/portfolio/ProjectCard";
 
 export default function PortfolioSection() {
   return (
-    <SectionContainer className="flex flex-col items-center gap-space-5 justify-center">
+    <SectionContainer className="flex flex-col items-center gap-space-7 justify-center">
       <SectionTitle caption="portfolio">Some of my previous works</SectionTitle>
-      <div className="grid grid-cols-1 md:grid-cols-2 max-w-md gap-space-5 md:max-w-[920px]">
+      <div className="grid grid-cols-1 px-space-3 md:grid-cols-2 max-w-md gap-space-5 md:max-w-[920px]">
         {projects.map((project) => (
           <ProjectCard key={project.id} project={project} />
         ))}
